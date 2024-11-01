0d5433b8b08a88221793be800a8fe76f14509b8d
luigi138 <nimapi20041012@gmail.com>
2024-11-01 20:25:54 -0300

    test 1 crud

---
+++ GrowerTech_MVC.Tests/GrowerTech_MVC.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GrowerTech_MVC.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // testando
+        }
+    }
+}
