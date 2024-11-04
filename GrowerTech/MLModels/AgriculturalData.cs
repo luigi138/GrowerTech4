75f50ef17625ecd9317cc82f1fbc7dde29464a89
luigi138 <nimapi20041012@gmail.com>
2024-11-03 22:43:57 -0300

    IA ML

---
+++ GrowerTech/MLModels/AgriculturalData.cs
@@ -0,0 +1,11 @@
+namespace GrowerTech_MVC.MLModels
+{
+    public class AgriculturalData
+    {
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float SoilPH { get; set; }
+        public float Rainfall { get; set; }
+        public string CropType { get; set; }
+    }
+}
---
+++ GrowerTech/MLModels/AgriculturalRecommendation.cs
@@ -0,0 +1,9 @@
+namespace GrowerTech_MVC.MLModels
+{
+    public class AgriculturalRecommendation
+    {
+        public string RecommendedInput { get; set; }
+        public float Confidence { get; set; }
+
+    }
+}
